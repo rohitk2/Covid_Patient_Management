f80bdcb1d388cdbe31790feabfebfa608908a649
rohitk2 <rohit98.kumar@gmail.com>
2021-04-11 22:38:15 -0500

    Update views.py

--- Patients/views.py
+++ Patients/views.py
@@ -20,6 +20,7 @@ def register(request) :
         form = UserCreationForm()
     return render(request, 'Patients/register.html', {'form': form})
 
+
 @login_required
 def user_page(request) :
     #get the patient who is logged in
@@ -28,15 +29,15 @@ def user_page(request) :
     #pull up the user data and data from the sensors
     Patient_user_info = User.objects.values()
     Patient_data = Patient_Data.objects.values()
+    #print(Patient_data)
+    #print(Patient_user_info)
 
     #Get the corresponding id for the username
     Patient = [(dict['username'], dict['id']) for dict in Patient_user_info if dict['username'] == patient][0]
-    full_data = [data for data in Patient_data if data['id'] == Patient[1]][0]
-    #Add the username to the data
-    full_data['username'] = Patient[0]
-    print(full_data)
-
+    full_data = [data for data in Patient_data if data['patient_id'] == Patient[1]]
+    #input the data in to the html page
     context = {
+        'user': Patient[0],
         'full_data': full_data
     }
     return render(request, 'Patients/user_page.html', context)
