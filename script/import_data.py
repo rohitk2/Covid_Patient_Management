7a93846a3a77862a2983a2ed8f7e186efcb29a7d
rohitk2 <rohit98.kumar@gmail.com>
2021-04-11 22:36:23 -0500

    Update import_data.py

--- script/import_data.py
+++ script/import_data.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
-import json
+
 from Patient_Management.models import Patient_Data
 from django.contrib.auth.models import User
 from datetime import datetime
 
 import json
-data1 = '{ "user":"user2", "Blood Pressure (Systolic)":100, "Blood Pressure (Diastolic)": 70, ' \
-        '  "Pulse Rate":72, "Temperature":40, "Blood Oxygen":97}'
-data1 = json.loads(data1)
 
+data1 = '{ "user":"user2", "Blood Pressure (Systolic)":99, "Blood Pressure (Diastolic)": 76, ' \
+        '  "Pulse Rate":93, "Temperature":42, "Blood Oxygen":88}'
+data1 = json.loads(data1)
 #print(User.objects.all())
 
 patient_username = data1["user"]
 user_ = User.objects.filter(username=patient_username)[0]
-
+#print((patient_username, user_))
 
 data1 = Patient_Data(temperature=data1['Temperature'], 
                      pulse=data1["Pulse Rate"],
